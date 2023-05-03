7d53e9a6b18dec6add11df1962a4a9ec30136cb4
rodrigues159 <57396876+rodrigues159@users.noreply.github.com>
2023-05-03 16:36:04 -0300

    Ajustes na forma de validação dum anime valido retornado

--- commands/animeinfo.js
+++ commands/animeinfo.js
@@ -1,5 +1,15 @@
 const axios = require('axios');
 
+
+async function isValidUrl(url) {
+  try {
+    const response = await axios.head(url);
+    return response.status === 200;
+  } catch (error) {
+    return false;
+  }
+}
+
 module.exports = {
   name: 'animeinfo',
   description: 'Mostra informações de um anime',
@@ -17,17 +27,21 @@ module.exports = {
     try {
 
       const response = await axios.get(`https://api.jikan.moe/v4/anime?q=${query}&limit=10`);
-      const animeList = response.data.data;
+      const results = response.data.data;
 
-      if (!animeList || animeList.length === 0) {
-        return interaction.reply('Anime não encontrado. Tente novamente com outro nome.');
-      }
-
-      const anime = animeList.find(
-        (item) =>
-          item.title.toLowerCase().includes(query.toLowerCase()) ||
-          (item.title_english && item.title_english.toLowerCase().includes(query.toLowerCase()))
+      const exactMatch = results.find(
+        (anime) =>
+          anime.title.toLowerCase() === query.toLowerCase() ||
+          (anime.title_english && anime.title_english.toLowerCase() === query.toLowerCase())
       );
+
+      const partialMatch = results.find(
+        (anime) =>
+          anime.title.toLowerCase().includes(query.toLowerCase()) ||
+          (anime.title_english && anime.title_english.toLowerCase().includes(query.toLowerCase()))
+      );
+
+      const anime = exactMatch || partialMatch;
 
       if (!anime) {
         return interaction.reply('Anime não encontrado. Tente novamente com outro nome.');
@@ -42,9 +56,9 @@ module.exports = {
       ? anime.synopsis.replace('[Written by MAL Rewrite]', `[Continue lendo no MyAnimeList](${malUrl})`)
       : 'N/A';
       const studio = animeDetails.studios && animeDetails.studios.length > 0
-      ? `[${animeDetails.studios[0].name}](${animeDetails.studios[0].url})`
-      : 'N/A';
-
+        ? animeDetails.studios[0].name
+        : 'N/A';
+      
       const infoEmbed = {
         color: 0x0099ff,
         title: anime.title,
