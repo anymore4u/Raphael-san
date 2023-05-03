12f516096d9ae5681edab1881930a815d6421eaa
rodrigues159 <57396876+rodrigues159@users.noreply.github.com>
2023-05-03 14:02:21 -0300

    Criação da função /animeinfo

--- commands/animeinfo.js
+++ commands/animeinfo.js
@@ -1,5 +1,4 @@
-const jikanjs  = require('jikanjs');
-const { Interaction } = require('discord.js');
+const axios = require('axios');
 
 module.exports = {
   name: 'animeinfo',
@@ -7,61 +6,81 @@ module.exports = {
   options: [
     {
       name: 'nome',
-      description: 'Nome do anime que deseja buscar',
       type: 3,
+      description: 'Nome do anime',
       required: true,
     },
   ],
-
-  async execute(interaction) {
-    await interaction.reply("Comando está em desenvolvimento");
-    const gifUrl = 'https://media.tenor.com/eAqD-5MDzFAAAAAC/mai-sakurajima-sakurajima-mai.gif';
-    await interaction.followUp(gifUrl);
-}
-
-  /*
   async execute(interaction) {
     const query = interaction.options.getString('nome');
 
-        try {
-            const animeSearchResult = await jikanjs.search('anime', query);
-            const animeId = animeSearchResult.results[0].mal_id;
-            const animeInfo = await jikanjs.loadAnime(animeId);
+    try {
+      const response = await axios.get(`https://api.jikan.moe/v4/anime?q=${query}&limit=1`);
+      const anime = response.data.data[0];
 
-            const embed = {
-                title: animeInfo.title,
-                url: animeInfo.url,
-                thumbnail: {
-                    url: animeInfo.image_url,
-                },
-                fields: [
-                    {
-                        name: 'Sinopse',
-                        value: animeInfo.synopsis,
-                    },
-                    {
-                        name: 'Tipo',
-                        value: animeInfo.type,
-                        inline: true,
-                    },
-                    {
-                        name: 'Episódios',
-                        value: animeInfo.episodes,
-                        inline: true,
-                    },
-                    {
-                        name: 'Média de notas',
-                        value: animeInfo.score,
-                        inline: true,
+      if (!anime) {
+        return interaction.reply('Anime não encontrado. Tente novamente com outro nome.');
+      }
 
+      const animeId = anime.mal_id;
+      const detailsResponse = await axios.get(`https://api.jikan.moe/v4/anime/${animeId}`);
+      const animeDetails = detailsResponse.data.data;
+
+      const malUrl = `https://myanimelist.net/anime/${anime.mal_id}`;
+      const description = anime.synopsis
+      ? anime.synopsis.replace('[Written by MAL Rewrite]', `[Continue lendo no MyAnimeList](${malUrl})`)
+      : 'N/A';
+      const studio = animeDetails.studios && animeDetails.studios.length > 0
+        ? animeDetails.studios[0].name
+        : 'N/A';
+      
+      const infoEmbed = {
+        color: 0x0099ff,
+        title: anime.title,
+        url: malUrl,
+        description: description,
+        thumbnail: {
+          url: anime.images.webp.image_url,
+        },
+        fields: [
+          {
+            name: 'Episódios',
+            value: anime.episodes ? anime.episodes.toString() : 'N/A',
+            inline: true,
+          },
+          {
+            name: 'Score',
+            value: anime.score ? anime.score.toString() : 'N/A',
+            inline: true,
+          },
+          {
+            name: 'Tipo',
+            value: anime.type ? anime.type.toString() : 'N/A',
+            inline: true,
+          },
+          {
+            name: 'Rank',
+            value: anime.rank ? anime.rank.toString() : 'N/A',
+            inline: true,
+          },
+          {
+            name: 'Status',
+            value: anime.status ? anime.status.toString() : 'N/A',
+            inline: true,
+          },
+          {
+            name: 'Studio',
+            value: studio,
+            inline: true,
           },
         ],
+        timestamp: new Date(),
       };
 
-      await interaction.reply({ embeds: [embed] });
+      interaction.reply({ embeds: [infoEmbed] });
     } catch (error) {
-      console.error(error);
-      await interaction.reply('Ocorreu um erro ao buscar o anime.');
+      console.error('Erro ao buscar informações do anime:', error);
+      interaction.reply('Ocorreu um erro ao buscar informações do anime. Tente novamente mais tarde.');
     }
-  },*/
+  },
 };
